cc70a939662e793e2ffcb6ac489e09367637a8dc
omote <info@mand-y.com>
2022-05-10 17:55:42 +0900

    音楽

---
+++ audio.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title('フィヨルドの風')
+
+audio_file = open('myaudio.mp3', 'rb')
+audio_bytes = audio_file.read()
+
+st.audio(audio_bytes, format='audio/mp3',start_time=0)
