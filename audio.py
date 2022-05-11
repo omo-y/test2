d48fec704faa82e9055debe37c41dff19eeb87aa
omo-y <46962034+omo-y@users.noreply.github.com>
2022-05-11 12:58:39 +0900

    Update audio.py

--- audio.py
+++ audio.py
@@ -3,9 +3,7 @@ import numpy as np
 import pandas as pd
 
 st.title('フィヨルドの風')
-button=st.button('聞きますか？')
-if button:
 
-  audio_file = open('myaudio.mp3', 'rb')
-  audio_bytes = audio_file.read()
-  st.audio(audio_bytes, format='audio/ogg')
+ audio_file = open('myaudio.mp3', 'rb')
+ audio_bytes = audio_file.read()
+ st.audio(audio_bytes, format='audio/ogg')
